9441324d7fe87ef36d87882a385aa9e697aaf0ba
Samuel Nascimento <sfsnascimento@gmail.com>
2021-07-11 23:51:07 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function adicionaNaLista () {
+function adicionaNaLista() {
   const botao = document.querySelector('#criar-tarefa');
 
-  botao.addEventListener('click', function () {
+  botao.addEventListener('click', function() {
     const entradaTexto = document.getElementById('texto-tarefa');
     const elementoPai = document.getElementById('lista-tarefas');
     const lista = document.createElement('li');
@@ -12,34 +12,45 @@ function adicionaNaLista () {
 }
 adicionaNaLista();
 
-function adicionaRetiraBackground () {
+function adicionaRetiraBackground() {
   const list = document.getElementById('lista-tarefas');
   list.addEventListener('click', function (event) {
-    let listaEl = document.getElementsByTagName('li');
+    const listaEl = document.getElementsByTagName('li');
     for (let index = 0; index < listaEl.length; index += 1) {
       listaEl[index].classList.remove('selected');
     }
     event.target.classList.add('selected');
   });
 }
-adicionaRetiraBackground ();
+adicionaRetiraBackground();
 
-function riscaItem () {
+function riscaItem() {
   const risca = document.getElementById('lista-tarefas');
-  risca.addEventListener('dblclick', function (event) {
+  risca.addEventListener('dblclick', function(event) {
     event.target.classList.toggle('completed');
   });
 }
-riscaItem ();
+riscaItem();
 
-function apagaTodosItems () {
+function apagaTodosItems() {
   const apagaEl = document.getElementById('apaga-tudo');
-  apagaEl.addEventListener('click', function () {
-//source:https://www.javascripttutorial.net/javascript-dom/javascript-removechild/
+  apagaEl.addEventListener('click', function() {
+    //source:https://www.javascripttutorial.net/javascript-dom/javascript-removechild/
     const elementoPai = document.getElementById('lista-tarefas');
     while (elementoPai.firstChild) {
       elementoPai.removeChild(elementoPai.firstChild);
     } 
   });
 }
-apagaTodosItems ();
+apagaTodosItems();
+
+function removeTarefasFinalizadas() {
+  const removeFinalizadas = document.getElementById('remover-finalizados');
+  removeFinalizadas.addEventListener('click', function() {
+    const finalizadas = document.getElementsByClassName('completed');
+    while (finalizadas[0]) {
+      finalizadas[0].remove();
+    } 
+  });
+}
+removeTarefasFinalizadas();
